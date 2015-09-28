152802a142f1a4f08f035e86d770c608d2e22a71
TO001UR <Justyna1>
2015-09-28 11:32:13 +0200

    Create wb-services.

---
+++ wb-services/src/test/com/tomeq/wb/resource/CityResourceTest.java
@@ -0,0 +1,16 @@
+package com.tomeq.wb.resource;
+
+import org.junit.Test;
+
+public class CityResourceTest {
+
+	@Test
+	public void can_get_all_cities() {
+
+	}
+
+	@Test
+	public void can_get_city() {
+
+	}
+}
